7700a113900f60f3095f501db01f433c13588637
yonggyo1125 <yonggyo00@kakao.com>
2023-05-22 10:32:11 +0900

    .

---
+++ src/main/java/org/koreait/models/board/config/BoardConfigSaveService.java
@@ -0,0 +1,55 @@
+package org.koreait.models.board.config;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.commons.constants.Role;
+import org.koreait.controllers.admins.BoardForm;
+import org.koreait.entities.Board;
+import org.koreait.repositories.BoardRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * 게시판 설정 추가, 수정
+ *
+ */
+@Service
+@RequiredArgsConstructor
+public class BoardConfigSaveService {
+
+    private final BoardRepository boardRepository;
+
+    public void save(BoardForm boardForm) {
+        /**
+         * 게시판 설정 조회 -> 없으면 엔티티 생성
+         */
+        String bId = boardForm.getBId();
+        Board board = boardRepository.findById(bId).orElseGet(Board::new);
+
+        board.setBId(bId);
+        board.setBName(boardForm.getBName());
+        board.setUse(boardForm.isUse());
+        board.setRowsOfPage(boardForm.getRowsOfPage());
+        board.setShowViewList(boardForm.isShowViewList());
+        board.setCategory(boardForm.getCategory());
+        board.setListAccessRole(Role.valueOf(boardForm.getListAccessRole()));
+
+        board.setViewAccessRole(Role.valueOf(boardForm.getViewAccessRole()));
+
+        board.setWriteAccessRole(Role.valueOf(boardForm.getWriteAccessRole()));
+
+        board.setReplyAccessRole(Role.valueOf(boardForm.getReplyAccessRole()));
+
+        board.setCommentAccessRole(Role.valueOf(boardForm.getCommentAccessRole()));
+
+        board.setUseEditor(boardForm.isUseEditor());
+        board.setUseAttachFile(boardForm.isUseAttachFile());
+        board.setUseAttachImage(boardForm.isUseAttachImage());
+
+        board.setLocationAfterWriting(boardForm.getLocationAfterWriting());
+
+        board.setUseReply(boardForm.isUseReply());
+        board.setUseComment(boardForm.isUseComment());
+        board.setSkin(boardForm.getSkin());
+
+
+    }
+}
