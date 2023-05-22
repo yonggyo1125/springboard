01e2b3aa042f2701ec2deeb016704774c82f01ec
yonggyo1125 <yonggyo00@kakao.com>
2023-05-22 09:35:14 +0900

    .

--- src/main/java/org/koreait/commons/constants/Role.java
+++ src/main/java/org/koreait/commons/constants/Role.java
@@ -1,6 +1,7 @@
 package org.koreait.commons.constants;
 
 public enum Role {
+    ALL, // 전체 사용자
     USER, // 일반 사용자
     ADMIN // 관리자
 }
---
+++ src/main/java/org/koreait/entities/Board.java
@@ -0,0 +1,60 @@
+package org.koreait.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.koreait.commons.constants.Role;
+
+@Entity @Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Board extends BaseMemberEntity {
+    private String bId; // 게시판 ID 
+    private String bName; // 게시판명
+
+    @Column(name="isUse")
+    private boolean use; // 사용 여부
+
+    private int rowsOfPage = 20; // 1페이지당 게시글 수
+
+    private boolean showViewList; // 게시글 하단 목록 노출
+
+    private String category; // 게시판 분류
+    
+    // 목록 접근 권한
+    private Role listAccessRole = Role.ALL;
+
+    // 글보기 접근 권한
+    private Role ViewAccessRole = Role.ALL;
+
+    // 글쓰기 접근 권한
+    private Role writeAccessRole = Role.ALL;
+
+    // 답글 접근 권한
+    private Role replyAccessRole = Role.ALL;
+
+    // 댓글 접근 권한
+    private Role commentAccessRole = Role.ALL;
+
+    // 에디터 사용 여부 
+    private boolean useEditor;
+    
+    // 파일 첨부 사용여부
+    private boolean useAttachFile;
+    
+    // 이미지 첨부 사용여부
+    private boolean useAttachImage;
+
+    // 글작성 후 이동
+    private String locationAfterWriting = "view";
+
+    // 답글 사용 여부
+    private boolean useReply;
+
+    // 댓글 사용 여부
+    private boolean useComment;
+
+    // 게시판 스킨
+    private String skin = "default";
+}
